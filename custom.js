82960346fa71c6c583018a886dbe941ba193c589
maheshanandabhilre <37931909+maheshanandabhilre@users.noreply.github.com>
2020-08-30 19:53:24 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.type_text', {
+  // Waits 1000ms after typing "First"
+  typeSpeed: 70,
+  smartBackspace: false,
+  loop: true,
+  loopCount: Infinity,
+  strings: ['I love web design', 'I love development','I love programming','I am programmer','I love Digital Marketting']
+});
+
+
+
+
+
+
